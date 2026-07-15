dacf2216f4367b6fa3f6397b2b183d4f39ca5f98
srishti200426 <srishtimukhija.26@gmail.com>
2026-07-16 03:00:57 +0530

    added set zero matrix  solution

---
+++ arrays/set_matrix0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row_len = len(matrix) 
+        col_len = len(matrix[0])
+
+        col0 = 1
+
+        for i in range(0,row_len):
+            for j in range(0,col_len):
+                if (matrix[i][j] == 0):
+                    matrix[i][0] = 0
+                    if(j!=0):
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+        
+        for i in range(1,row_len):
+            for j in range(1,col_len):
+                if(matrix[i][j] != 0):
+                    if(matrix[i][0] == 0 or matrix[0][j] == 0):
+                        matrix[i][j] = 0
+
+        if(matrix[0][0] == 0):
+            for j in range(0,col_len):
+                matrix[0][j] = 0
+
+        if col0 == 0:
+            for i in range(0,row_len):
+                matrix[i][0] = 0
+
+        
+
+
+
+        
